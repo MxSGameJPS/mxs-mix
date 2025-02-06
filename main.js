29da3653bd20887e5f66811fdcfb7e2383eca323
MxSGameJPS <mxsgamejps@gmail.com>
2025-02-06 08:59:45 -0300

    finalização

---
+++ main.js
@@ -0,0 +1,24 @@
+function tocaSom(idTagAudio) {
+  document.querySelector(idTagAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (event) {
+    if (event.code === "Space" || event.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };     
+}
